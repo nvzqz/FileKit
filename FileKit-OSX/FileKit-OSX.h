20d8bcf986d387a2b72e26522ddd606997e9fb02
Nikolai Vazquez <nvazquez1297@gmail.com>
2015-09-02 00:55:05 -0400

    Add OS X and iOS build targets

---
+++ FileKit-OSX/FileKit-OSX.h
@@ -0,0 +1,19 @@
+//
+//  FileKit-OSX.h
+//  FileKit-OSX
+//
+//  Created by Nikolai Vazquez on 9/2/15.
+//  Copyright © 2015 Nikolai Vazquez. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FileKit-OSX.
+FOUNDATION_EXPORT double FileKit_OSXVersionNumber;
+
+//! Project version string for FileKit-OSX.
+FOUNDATION_EXPORT const unsigned char FileKit_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FileKit_OSX/PublicHeader.h>
+
+
---
+++ FileKit-iOS/FileKit-iOS.h
@@ -0,0 +1,19 @@
+//
+//  FileKit-iOS.h
+//  FileKit-iOS
+//
+//  Created by Nikolai Vazquez on 9/2/15.
+//  Copyright © 2015 Nikolai Vazquez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FileKit-iOS.
+FOUNDATION_EXPORT double FileKit_iOSVersionNumber;
+
+//! Project version string for FileKit-iOS.
+FOUNDATION_EXPORT const unsigned char FileKit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FileKit_iOS/PublicHeader.h>
+
+
