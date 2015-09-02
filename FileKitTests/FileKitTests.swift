ab3fca82813d58efcdf720399488d84ca8857598
Nikolai Vazquez <nvazquez1297@gmail.com>
2015-09-01 22:30:53 -0400

    Create FileKitTests build target for unit testing

---
+++ FileKitTests/FileKitTests.swift
@@ -0,0 +1,13 @@
+//
+//  FileKitTests.swift
+//  FileKitTests
+//
+//  Created by Nikolai Vazquez on 9/1/15.
+//  Copyright © 2015 Nikolai Vazquez. All rights reserved.
+//
+
+import XCTest
+
+class FileKitTests: XCTestCase {
+    
+}
