4cb4b2c188e5cff8a3f806ea64f4b017b5c70889
Nikolai Vazquez <nvazquez1297@gmail.com>
2015-09-01 21:52:31 -0400

    Create empty File class

---
+++ FileKit/FileKit.swift
@@ -0,0 +1,13 @@
+//
+//  FileKit.swift
+//  FileKit
+//
+//  Created by Nikolai Vazquez on 9/1/15.
+//  Copyright © 2015 Nikolai Vazquez. All rights reserved.
+//
+
+import Foundation
+
+class File {
+    
+}
