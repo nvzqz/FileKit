da245b9b0574554cb058a31a0b54fd708a4dad10
Nikolai Vazquez <nvazquez1297@gmail.com>
2015-09-02 00:59:39 -0400

    Consolidate build targets

---
+++ FileKit/FileKit.h
@@ -0,0 +1,19 @@
+//
+//  FileKit.h
+//  FileKit
+//
+//  Created by Nikolai Vazquez on 9/2/15.
+//  Copyright © 2015 Nikolai Vazquez. All rights reserved.
+//
+
+@import Foundation;
+
+//! Project version number for FileKit-OSX.
+FOUNDATION_EXPORT double FileKitVersionNumber;
+
+//! Project version string for FileKit-OSX.
+FOUNDATION_EXPORT const unsigned char FileKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FileKit/PublicHeader.h>
+
+
