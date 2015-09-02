0dc509fb31b54f3fa265abaafae99f32afedb927
Nikolai Vazquez <nvazquez1297@gmail.com>
2015-09-01 21:55:28 -0400

    Create empty Path struct

---
+++ FileKit/Path.swift
@@ -0,0 +1,13 @@
+//
+//  Path.swift
+//  FileKit
+//
+//  Created by Nikolai Vazquez on 9/1/15.
+//  Copyright © 2015 Nikolai Vazquez. All rights reserved.
+//
+
+import Foundation
+
+struct Path {
+    
+}
