cf478a79e0dfb68538f64692c1850ecfcac06b87
Nikolai Vazquez <nvazquez1297@gmail.com>
2015-09-02 01:03:26 -0400

    Create unit tests for OS X and iOS

---
+++ FileKit-OSX-Tests/FileKit_OSX_Tests.swift
@@ -0,0 +1,13 @@
+//
+//  FileKit_OSX_Tests.swift
+//  FileKit-OSX-Tests
+//
+//  Created by Nikolai Vazquez on 9/2/15.
+//  Copyright © 2015 Nikolai Vazquez. All rights reserved.
+//
+
+import XCTest
+
+class FileKit_OSX_Tests: XCTestCase {
+    
+}
---
+++ FileKit-iOS-Tests/FileKit_iOS_Tests.swift
@@ -0,0 +1,13 @@
+//
+//  FileKit_iOS_Tests.swift
+//  FileKit-iOS-Tests
+//
+//  Created by Nikolai Vazquez on 9/2/15.
+//  Copyright © 2015 Nikolai Vazquez. All rights reserved.
+//
+
+import XCTest
+
+class FileKit_iOS_Tests: XCTestCase {
+    
+}
