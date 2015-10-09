b47ce8e18f790bfd1b01f12d2d364c522d3e71ad
Nikolai Vazquez <nvazquez1297@gmail.com>
2015-10-09 01:57:42 -0400

    Conform Foundation types to FKWritableToFile
    Types: NSDictionary, NSArray, and NSData

---
+++ FileKit/Extensions/Foundation+FileKit.swift
@@ -0,0 +1,13 @@
+//
+//  Foundation+FileKit.swift
+//  FileKit
+//
+//  Created by Nikolai Vazquez on 10/9/15.
+//  Copyright © 2015 Nikolai Vazquez. All rights reserved.
+//
+
+import Foundation
+
+extension NSDictionary: FKWritableToFile {}
+extension NSArray:      FKWritableToFile {}
+extension NSData:       FKWritableToFile {}
