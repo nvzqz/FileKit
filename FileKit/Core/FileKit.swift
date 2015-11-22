604bb09f0e4cef4e9f37e0e5a3276a8737466b3b
Nikolai Vazquez <nvazquez1297@gmail.com>
2015-11-21 21:41:58 -0500

    Add FileKit info as FileKit struct

---
+++ FileKit/Core/FileKit.swift
@@ -0,0 +1,48 @@
+//
+//  FileKit.swift
+//  FileKit
+//
+//  The MIT License (MIT)
+//
+//  Copyright (c) 2015 Nikolai Vazquez
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+/// Information regarding [FileKit](https://github.com/nvzqz/FileKit).
+public struct FileKit {
+
+    /// The current version.
+    ///
+    /// FileKit follows [Semantic Versioning v2.0.0](http://semver.org/).
+    public static let version = "v1.7.0"
+
+    /// The current release.
+    public static let release = 8
+
+    /// FileKit is licensed under the [MIT License](https://opensource.org/licenses/MIT).
+    public static let license = "MIT"
+
+    /// A brief description of FileKit.
+    public static let description = "A Swift framework that allows for simple and expressive file management."
+
+    /// Where the project can be found.
+    public static let projectURL = "https://github.com/nvzqz/FileKit"
+
+}
