ef40be1fb2e8cc8b8418f513798e0ef266241b9f
Nikolai Vazquez <nvazquez1297@gmail.com>
2016-05-07 23:58:21 -0400

    Add sample for writing files in FileKit.playground

--- FileKit.playground/Contents.swift
+++ FileKit.playground/Contents.swift
@@ -4,3 +4,13 @@ Use this playground to try out FileKit
 */
 import Cocoa
 import FileKit
+import XCPlayground
+
+extension Path {
+    static let SharedPlaygroundData = Path(url: XCPlaygroundSharedDataDirectoryURL)!
+}
+
+let shared = Path.SharedPlaygroundData
+let sample = TextFile(path: shared/"filekit_sample.txt")
+try? sample.write("Hello there!")
+try? sample.read()
