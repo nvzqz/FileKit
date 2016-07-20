3f11c0e96d8f07607b112dbc14826e0e7f972823
Nikolai Vazquez <nvazquez1297@gmail.com>
2016-07-20 19:14:32 -0400

    Prepare 3.0.0 release

--- README.md
+++ README.md
@@ -59,7 +59,7 @@ to learn more.
     ```ruby
     use_frameworks!
 
-    pod 'FileKit', '~> 2.0.0'
+    pod 'FileKit', '~> 3.0.0'
     ```
 
 2. Run `pod install` and open the `.xcworkspace` file to launch Xcode.
--- Sources/FileKit.swift
+++ Sources/FileKit.swift
@@ -33,7 +33,7 @@
 ///
 /// - Copyright: [MIT License](https://opensource.org/licenses/MIT)
 ///
-/// - Version: [v2.0.0](https://github.com/nvzqz/FileKit/releases/tag/v2.0.0)
+/// - Version: [v3.0.0](https://github.com/nvzqz/FileKit/releases/tag/v3.0.0)
 ///
 /// - Requires: Xcode 7+, Swift 2.0+
 ///
@@ -42,10 +42,10 @@ public enum FileKit {
     /// The current version.
     ///
     /// FileKit follows [Semantic Versioning v2.0.0](http://semver.org/).
-    public static let version = "v2.0.0"
+    public static let version = "v3.0.0"
 
     /// The current release.
-    public static let release = 9
+    public static let release = 10
 
     /// FileKit is licensed under the [MIT License](https://opensource.org/licenses/MIT).
     public static let license = "MIT"
