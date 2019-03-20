f6eb51edaa47853e11e22779c565a1c9b968f6b9
Davide Mazzoni <davidemazzoni@yahoo.it>
2019-03-20 12:14:46 +0100

    Update Package.swift to API v4.2

--- Package.swift
+++ Package.swift
@@ -1,3 +1,4 @@
+// swift-tools-version:4.2
 //
 //  Package.swift
 //  FileKit
@@ -29,7 +30,23 @@ import PackageDescription
 
 let package = Package(
     name: "FileKit",
-    exclude: [
-	"Tests"
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "FileKit",
+            targets: ["FileKit"]),
+    ],
+    dependencies: [],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "FileKit",
+            dependencies: [],
+            path: "Sources"),
+        .testTarget(
+            name: "FileKitTests",
+            dependencies: ["FileKit"],
+            path: "Tests")
     ]
 )
